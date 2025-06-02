b28523159ab5d310916e318b22f10f73dbb58c69
Emrah6158 <emrahozdemir92@hotmail.com>
2025-06-02 05:34:35 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'Arial', padding: 20 }}>
+      <h1>Staffadvisor'a Hoşgeldiniz 👋</h1>
+      <p>Bu demo klasik Next.js yapısıyla hazırlanmıştır ve Vercel ile %100 uyumludur.</p>
+    </div>
+  );
+}
