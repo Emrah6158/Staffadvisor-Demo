f1a31ae1dfe4bd686a320484434161f76ec2c2b8
Emrah6158 <emrahozdemir92@hotmail.com>
2025-06-02 05:44:53 +0300

    Delete page.tsx

--- page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <main style={{ padding: 20, fontFamily: 'Arial' }}>
-      <h1>Staffadvisor'a Hoşgeldiniz 👋</h1>
-      <p>Bu versiyon App Router uyumlu ve Vercel'de sorunsuz çalışır.</p>
-    </main>
-  );
-}
